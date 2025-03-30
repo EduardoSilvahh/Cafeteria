62aed4d338140f23d95bc272db4b75e045653567
EduardoSilvahh <eduardothobias13@gmail.com>
2025-03-30 16:46:09 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,19 @@
+const modal = document.querySelector(".modal")
+const mascara = document.querySelector(".mascara2")
+const sobre = document.querySelector(".caixaSobre")
+
+function mostrarModal (){
+    modal.style.left = '50%'
+    mascara.style.visibility = 'visible'
+}
+
+function esconderModal (){
+    modal.style.left = '-30%'
+    mascara.style.visibility = 'hidden'
+    
+}
+
+
+function mostrarSobre (){
+    sobre.style.caixaSobre
+}
